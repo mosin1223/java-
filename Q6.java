978f7f93ae56a08ba234b92e93a1f77e4867e12d
mosin1223 <cadetmohsan@gmail.com>
2024-02-24 00:14:35 +0500

    Create Q6.java

---
+++ Q6.java
@@ -0,0 +1,19 @@
+class ArayUtil{
+   static int i;
+    static int maximum(int[] array){
+         int max=array[0];
+        for(i=0;i<array.length;i++){
+           if(array[i]>max){
+               max=array[i];
+           }
+           else continue;
+
+        }return max;
+    }
+}
+public class Q6 {
+    public static void main(String args[]){
+        int[] array = {10,20,30,40,50};
+        System.out.println("The max num from array is : "+ ArayUtil.maximum(array));
+    }
+}
